c6d5fb233de8a8493e86f7296bd80b1edce824c7
atharvsawant2003 <atharvsawant2020@gmail.com>
2023-06-24 14:55:22 +0530

    made student and teacher portals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,22 @@ import AboutUs from './Components_1/about';
 import AboutUs1 from './Components_1/aboutUs';
 import Contact from './Components_1/contact';
 import Gallery from './Components_1/gallery';
+import SPortal from './Components_1/SPortal';
+import TPortal from './Components_1/TPortal';
 
 function App() {
   return (
     <>
       <Router>
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/about1" element={<AboutUs1 />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/student-portal" element={<SPortal/>} />
+          <Route path="/teacher-portal" element={<TPortal/>} />
           <Route path="*" element={<Navigate to="/" />} /> {/* Default fallback route */}
         </Routes>
       </Router>
