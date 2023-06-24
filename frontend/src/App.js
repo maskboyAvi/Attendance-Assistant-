b3cde74c4229df3ede9f9ff41dfe614dc74bd2f8
Pavana Saketha Ram <pavan.sakethram@gmail.com>
2023-06-24 16:33:54 +0530

    Update: React JS Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
 // import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './Components_1/home';
 import AboutUs from './Components_1/about';
 import AboutUs1 from './Components_1/aboutUs';
 import Contact from './Components_1/contact';
 import Gallery from './Components_1/gallery';
+import Portals from './Components_1/portals';
 import SPortal from './Components_1/SPortal';
 import TPortal from './Components_1/TPortal';
-// import Home from './Components_1/home';
-// import AboutUs from './Components_1/about';
-// import AboutUs1 from './Components_1/aboutUs';
-// import Contact from './Components_1/contact';
-// import Gallery from './Components_1/gallery';
-import Portals from './Components_1/portals';
 
 function App() {
   return (
     <>
-      <Router>
-        
+      <Router>        
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/about1" element={<AboutUs1 />} />
           <Route path="/contact" element={<Contact />} />
