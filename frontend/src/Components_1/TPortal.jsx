7280237809eb3c7ab3a6e52df8c33c46189b3ede
maskboyAvi <aviralofficial1729@gmail.com>
2023-07-08 13:14:20 +0530

    Fixed the teacher css

--- frontend/src/Components_1/TPortal.jsx
+++ frontend/src/Components_1/TPortal.jsx
@@ -1,74 +1,120 @@
-import React from 'react';
+import React from "react";
 import SPortalCss from "./SPortal.module.css";
-import myImageT from '../Images/teach.png';
-import NavPortal from './Nav-Portal';
-import Footer from './footer';
-import TableComponent from './Table';
+import myImageT from "../Images/teach.png";
+import NavPortal from "./Nav-Portal";
+import Footer from "./footer";
+import TableComponent from "./Table";
 
+import studImg from "../Images/SPortal_stud.png";
+import inputCss from "./Input.module.css";
 function TPortal() {
-
   function create() {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < 6) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
       counter += 1;
     }
-    document.getElementById("code-T").innerHTML = result;
+    // document.getElementById("code-T").innerHTML = result;
+    var inputElement = document.getElementById("code-T");
+    inputElement.disabled = false; // Remove the disabled attribute temporarily
+    inputElement.value = result; // Update the value
+    inputElement.disabled = true; // Reapply the disabled attribute
   }
 
   const end = () => {
     fetch("http://localhost:8080/newday", {
-      method: 'POST',
+      method: "POST",
     })
-      .then(response => response.text())
-      .then(result => {
+      .then((response) => response.text())
+      .then((result) => {
         console.log(result);
       })
-      .catch(error => {
-        console.log('Error:', error);
+      .catch((error) => {
+        console.log("Error:", error);
       });
-
-  }
+  };
 
   const ata = () => {
-    document.getElementById("atte").innerHTML = "RollNo of Present Student : "
+    document.getElementById("atte").innerHTML = "RollNo of Present Student : ";
     fetch("http://localhost:8080/check")
-      .then(response => response.json())
-      .then(result => document.getElementById("atte").innerHTML += result)
-      .catch(error => console.log('error', error));
-  }
+      .then((response) => response.json())
+      .then((result) => (document.getElementById("atte").innerHTML += result))
+      .catch((error) => console.log("error", error));
+  };
+    const divStyle = {
+      color: 'white',
+    };
   return (
     <>
       <NavPortal />
-      <div className='main-div-tportal'>
-        <h1 id="std">Welcome to Teacher  Portal</h1>
+      <div className="main-div-tportal">
+        <div className={SPortalCss["main-div-tportal"]}>
+          <section className={SPortalCss["hero"]}>
+            <div className={SPortalCss["hero-text"]}>
+              <h4>&#183; Attendance Assistant &#183;</h4>
+              <h1>Welcome to Teacher Portal</h1>
+              <p>
+                <strong>Never Miss a Beat</strong>
+                <br /> Stay on Top of Your Attendance With our state-of-the-art
+                student portal attendance system, we make attendance tracking a
+                breeze. Say goodbye to traditional paper-based methods and
+                embrace a seamless and efficient way to monitor your attendance
+                record.
+              </p>
+            </div>
+            <div className={SPortalCss["hero-pic"]}>
+              <img src={studImg} alt="Hero_Image" />
+            </div>
+          </section>
 
-        <div id="al" class="row">
-          <div class="col-6" >
-            <h1 style={{ marginBottom: "-1rem", marginLeft: "1rem" }}>Create Code </h1>
-            <button id="btn-t" onClick={create}>Create</button>
-            <button onClick={end} id="btn-2-t">Start Time</button>
-            <p id="code-T" style={{ marginLeft: "1rem" }}> </p>
-          </div>
-          <div>
-            <img src={myImageT} alt="tec" id="tec" />
-          </div>
-
-        </div>
-        <button onClick={ata} id="btn-2-t">Check Attendance</button>
-        <div id="atte">
-
-        </div>
-        <div>
-          <TableComponent />
+          <section>
+            <div className={SPortalCss["stud-content"]}>
+              <h1 className={SPortalCss["stud-heading"]}>
+                &#183; TAKE ATTENDANCE &#183;
+              </h1>
+              <form class={inputCss.form}>
+                
+                <div className={SPortalCss["btn-manage-sp"]}>
+                  <button
+                    class={inputCss.submit}
+                    onClick={create}
+                    type="button"
+                  >
+                    Create Code
+                  </button>
+                  <span class={inputCss["input-span"]}>
+                    <input
+                      type="text"
+                      name="code"
+                      id="code-T"
+                      value={"Eg. 3FTc8d"}
+                      disabled
+                      style={divStyle}
+                    />
+                  </span>
+                </div>
+                <button class={inputCss.submit} onClick={end} type="button">
+                  Start Attendance
+                </button>
+                <button class={inputCss.submit} onClick={ata} type="button">
+                  Check Attendance
+                </button>
+                <div id="atte"></div>
+                <div>
+                  <TableComponent />
+                </div>
+              </form>
+            </div>
+          </section>
         </div>
       </div>
       <Footer />
     </>
   );
-};
+}
 
 export default TPortal;
