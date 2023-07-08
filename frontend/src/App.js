f752e380c92479c9a8c7ce321609f00ea154e19d
Your Name <you@example.com>
2023-07-08 13:56:30 +0530

    Made techer login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import SLogin from './Components_1/s_login';
 import SPortal from './Components_1/SPortal';
 import TPortal from './Components_1/TPortal';
 import PhotoUpload from './Components_1/PhotoUpload';
+import TLogin from './Components_1/TLogin';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/portals" element={<Portals />} />
           <Route path="/student-login" element={<SLogin/>} />
+          <Route path="/teacher-login" element={<TLogin />} />
           <Route path="/student-portal" element={<SPortal/>} />
           <Route path="/teacher-portal" element={<TPortal/>} />
           <Route path="/photoupload" element={<PhotoUpload/>} />
